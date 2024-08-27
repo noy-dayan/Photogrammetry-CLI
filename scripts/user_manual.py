ab7b682608bf753039e26cf79c991f9d1c40e940
Noy Dayan <106970874+noy-dayan@users.noreply.github.com>
2024-08-27 17:10:35 +0300

    Update user_manual.py

--- scripts/user_manual.py
+++ scripts/user_manual.py
@@ -15,7 +15,7 @@ def intro_to_string():
 
 def v2i_help():
     print("""\033[35m═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\033[0m
-    \033[35mUsage:\033[0m video2images (or v2i) "\033[32m<video_path (must be in .mp4 format)>\033[0m" "\033[32m<project_path (creates a file named 'images' inside)>\033[0m" \033[36m[max_frames] [max_overlap_percentage] [ssim_threshold]\033[0m
+    \033[35mUsage:\033[0m video2images (or v2i) \033[32m<video_path (must be in .mp4 format)>\033[0m \033[32m<project_path (creates a file named 'images' inside)>\033[0m \033[36m[max_frames] [max_overlap_percentage] [ssim_threshold]\033[0m
     
     \033[35mDescription:\033[0m
     Extract frames from a video file and save them as images.
@@ -28,13 +28,13 @@ def v2i_help():
     \033[36m[ssim_threshold]\033[0m: SSIM threshold for frame similarity (default: 0.95).
           
     \033[35mExample:\033[0m
-    video2images "\033[32mpath\\video.mp4\033[0m" "\033[32mpath\\project\\dir\033[0m" \033[36m200 5 0.92\033[0m
+    v2i \033[32m"path\\video.mp4" "path\\project\\dir"\033[0m \033[36m200 5 0.92\033[0m
 \033[35m═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\033[0m""")
 
 
 def gpc_help():
     print("""\033[35m═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\033[0m
-    \033[35mUsage:\033[0m generatePointCloud (or gpc) "\033[32m<project_path (must contain a folder named 'images')>\033[0m"
+    \033[35mUsage:\033[0m generatePointCloud (or gpc) \033[32m<project_path (must contain a folder named 'images')>\033[0m
 
     \033[35mDescription:\033[0m
     Generate a point cloud (.ply) from a set of images.
@@ -43,13 +43,13 @@ def gpc_help():
     \033[32m<project_path>\033[0m: Path to the project folder containing the images folder.
 
     \033[35mExample:\033[0m
-    generatePointCloud "\033[32mpath\\project\\dir\033[0m"
+    gpc \033[32m"path\\project\\dir"\033[0m
 \033[35m═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\033[0m""")
 
 
 def cpc_help():
     print("""\033[35m═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\033[0m
-    \033[35mUsage:\033[0m combinePointClouds (or cpc) "\033[32m<cloud1_path>\033[0m" "\033[32m<cloud2_path>\033[0m" "\033[32m<output_path>\033[0m"
+    \033[35mUsage:\033[0m combinePointClouds (or cpc) \033[32m<cloud1_path>\033[0m \033[32m<cloud2_path>\033[0m \033[32m<output_path>\033[0m \033[36m[icp_iterations]\033[0m \033[36m[icp_overlap]\033[0m
 
     \033[35mDescription:\033[0m
     Combine two point clouds into a single point cloud using ICP.
@@ -58,18 +58,20 @@ def cpc_help():
     \033[32m<cloud1_path>\033[0m: Path to the first point cloud file.
     \033[32m<cloud2_path>\033[0m: Path to the second point cloud file.
     \033[32m<output_path>\033[0m: Path to the cloud output file.
-
+    \033[36m[icp_iterations]\033[0m: Number of iterations for ICP alignment (default: 10,000).
+    \033[36m[icp_overlap]\033[0m: Overlap percentage for ICP (default: 40). Must be an integer between 10 and 100.
+    
     \033[35mExample:\033[0m
-    cpc "\033[32mpath\\cloud1.ply\033[0m" "\033[32mpath\\cloud2.ply\033[0m"
+    cpc \033[32m"path\\cloud1.ply" "path\\cloud2.ply"\033[0m \033[36m200 5000 30\033[0m
 \033[35m═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\033[0m""")
 
 
 def general_help():
     print("""\033[35m═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\033[0m
     \033[35mAvailable commands:\033[0m
-    \033[35m>\033[0m video2images (or v2i) "\033[32m<video_path (must be in .mp4 format)>"\033[0m" "\033[32m<project_path (creates a file named 'images' inside)>\033[0m" \033[36m[max_frames] [max_overlap_percentage] [ssim_threshold]\033[0m
-    \033[35m>\033[0m generatePointCloud (or gpc) "\033[32m<project_path (must contain a folder named 'images')>\033[0m"
-    \033[35m>\033[0m combinePointClouds (or cpc) "\033[32m<cloud1_path>\033[0m" "\033[32m<cloud2_path>\033[0m" "\033[32m<output_path>\033[0m"
+    \033[35m>\033[0m video2images (or v2i) \033[32m<video_path (must be in .mp4 format)>\033[0m \033[32m<project_path (creates a file named 'images' inside)>\033[0m \033[36m[max_frames] [max_overlap_percentage] [ssim_threshold]\033[0m
+    \033[35m>\033[0m generatePointCloud (or gpc) \033[32m<project_path (must contain a folder named 'images')>\033[0m
+    \033[35m>\033[0m combinePointClouds (or cpc) \033[32m<cloud1_path>\033[0m \033[32m<cloud2_path>\033[0m \033[32m<output_path>\033[0m \033[36m[icp_iterations]\033[0m \033[36m[icp_overlap]\033[0m
 
     To get detailed help for a specific command, Type: \033[32m<command>\033[0m \033[35m-help\033[0m
     Type \033[31m-exit\033[0m to close the program.
